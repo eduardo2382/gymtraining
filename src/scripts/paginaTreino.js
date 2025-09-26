9300d15bf921b17d78f7fee4c45676872677c24b
eduardo2382 <motadossantosjoseeduardo.m822gmail.com>
2025-09-26 19:22:56 -0300

    layout do editExercicio criado

--- src/scripts/paginaTreino.js
+++ src/scripts/paginaTreino.js
@@ -37,7 +37,7 @@ function validarInput(inputs){
 
 function limparInput(inputs){
     inputs.forEach((input)=>{
-        input.value = ""
+        input.value = input.type == 'number' ? 0 : ""
     })
 }
 
@@ -66,35 +66,63 @@ function mostrarModalNovoExercicio(callback){
     }
 }
 
+
 function criarElementoExercicio(exercicio){
     let exercicioElemento = document.createElement('div')
     exercicioElemento.setAttribute('class', 'exercicio')
 
     exercicioElemento.innerHTML = `
-        <span class="exercicioHeader">
-            <h2>${exercicio.nome}</h2>
-            <i class="ri-pencil-fill"></i>
-        </span>
-        <span class="exercicioContent">
-            <div class="divInformacao">
-                <span class="infoTitulo">Séries</span>
-                <span class="serie info">${exercicio.series}</span>
-            </div>
-            <div class="divInformacao">
-                <span class="infoTitulo">Repetições</span>
-                <span class="repeticao info">${exercicio.repeticoes}</span>
-            </div>
-            <div class="divInformacao">
-                <span class="infoTitulo">Peso (kg)</span>
-                <span class="peso info">${exercicio.peso}</span>
-            </div>
-        </span>
+         <div class="contentExercicio">
+                    <span class="exercicioHeader">
+                        <h2>Supino Reto</h2>
+                        <i class="ri-pencil-fill"></i>
+                    </span>
+                    <span class="exercicoInfos">
+                        <div class="divInformacao">
+                            <span class="infoTitulo">Séries</span>
+                            <span class="serie info">0</span>
+                        </div>
+                        <div class="divInformacao">
+                            <span class="infoTitulo">Repetições</span>
+                            <span class="repeticao info">0</span>
+                        </div>
+                        <div class="divInformacao">
+                            <span class="infoTitulo">Peso (kg)</span>
+                            <span class="peso info">0</span>
+                        </div>
+                    </span>
+                </div>
+                <div class="editExercicio hidden">
+                    <span class="editExercicioHeader">
+                        <input type="text" id="inputNomeEditExercicio" placeholder="Nome do Exercicio:">
+                        <span class="btnsEdit">
+                            <i class="ri-delete-bin-fill btnEditDelete"></i>
+                            <i class="ri-check-line btnEditConfirm"></i>
+                        </span>
+                    </span>
+                    <span class="exercicoInfos">
+                        <div class="divInformacao">
+                            <span class="infoTitulo">Séries</span>
+                            <input type="number" id="editSeries" onkeypress="return event.charCode >= 48 && event.charCode <= 57" oninput="event.target.value <0 ? event.target.value=0 : undefined" value="0">
+                        </div>
+                        <div class="divInformacao">
+                            <span class="infoTitulo">Repetições</span>
+                            <input type="number" id="editRepeticoes" onkeypress="return event.charCode >= 48 && event.charCode <= 57" oninput="event.target.value <0 ? event.target.value=0 : undefined" value="0">
+                        </div>
+                        <div class="divInformacao">
+                            <span class="infoTitulo">Peso (kg)</span>
+                            <input type="number" id="editPeso" onkeypress="return event.charCode >= 48 && event.charCode <= 57" oninput="event.target.value <0 ? event.target.value=0 : undefined" value="0">
+                        </div>
+                    </span>
+                </div>
     `
 
     document.querySelector("#content").appendChild(exercicioElemento)
 
     return exercicioElemento
 }
+
+
 
 function atualizarExercicios(){
     let exercicios = treinoAtual.exercicios
